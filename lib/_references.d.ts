d79c42a6309b67117ed8bb9f69ae7b48134afb7c
Mathew Henson <mat@webinate.net>
2017-04-21 00:09:51 +0100

    Removed es6 promise definition

--- lib/_references.d.ts
+++ lib/_references.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="./definitions/required/angular.d.ts" />
 /// <reference path="./definitions/required/angular-ui-router.d.ts" />
 /// <reference path="./definitions/required/jquery.d.ts" />
-/// <reference path="./definitions/required/es6-promise.d.ts" />
 /// <reference path="./definitions/required/node.d.ts" />
 /// <reference path="./definitions/required/express.d.ts" />
 /// <reference path="./definitions/required/modepress-api.d.ts" />
