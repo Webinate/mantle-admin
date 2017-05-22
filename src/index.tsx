a89070115b27518ce1987f3a49d9c47f8eb8821e
Mathew Henson <mat@webinate.net>
2017-05-22 20:11:49 +0100

    Created simple webpack config for project

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+require( "./css/common.scss" );
+
+export const app = ReactDOM.render( <div>Test</div>, document.getElementById( 'root' ) );
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join( __dirname, 'dist' )
+  },
+  plugins: [
+    new webpack.DefinePlugin( {
+      'process.env': {
+        NODE_ENV: JSON.stringify( 'development' )
+      }
+    } ),
+    new HtmlWebpackPlugin( {
+      template: './src/index.html',
+      filename: 'index.html'
+    } ),
+    new ExtractTextPlugin( {
+      filename: '[name].css',
+      allChunks: true
+    } )
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      {
+        enforce: 'pre',
+        test: /\.tsx?$/,
+        use: "source-map-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract( [ 'css-loader', 'sass-loader' ] )
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ ".tsx", ".ts", ".js" ]
+  },
+  devtool: 'inline-source-map'
+};
