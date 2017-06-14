7fcc77ce2c54cdf127d3cd42b5cf5ff1fc190bba
Mathew Henson <mat@webinate.net>
2017-06-14 17:47:41 +0100

    Updated modepress dist folder to client

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -1,27 +1,18 @@
 const path = require( 'path' );
 const webpack = require( 'webpack' );
-const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 
 module.exports = {
   entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
-    path: path.join( __dirname, 'dist/public' )
+    path: path.join( __dirname, 'dist/client' )
   },
   plugins: [
     new webpack.DefinePlugin( {
       'process.env': {
         NODE_ENV: JSON.stringify( 'development' )
       }
-    } ),
-    // new HtmlWebpackPlugin( {
-    //   template: './src/index.html',
-    //   filename: 'index.html'
-    // } ),
-    new ExtractTextPlugin( {
-      filename: '[name].css',
-      allChunks: true
     } )
   ],
   module: {
@@ -40,19 +31,11 @@ module.exports = {
         enforce: 'pre',
         test: /\.tsx?$/,
         use: "source-map-loader"
-      },
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract( [ 'css-loader', 'sass-loader' ] )
       }
     ]
   },
   resolve: {
-    extensions: [ ".tsx", ".ts", ".js" ]
+    extensions: [ "*", ".tsx", ".ts", ".js" ]
   },
   devtool: 'inline-source-map'
 };
