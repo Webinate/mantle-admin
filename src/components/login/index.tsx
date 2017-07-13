b628543f0744eb09d17b7387ddb6109144a0d2fa
Mathew Henson <mat@webinate.net>
2017-07-13 23:50:04 +0100

    Improved CSS & added first state checks

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,30 +1,39 @@
 import * as React from "react";
-import { FlatButton, RaisedButton, TextField, FontIcon } from "material-ui";
+import { RaisedButton, TextField, FontIcon } from "material-ui";
 
-export class LoginForm extends React.Component<any, any> {
+export class LoginForm extends React.Component<any, { user: string; pass: string; }> {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      pass: ''
+    }
+  }
+
+  private retrievePass() {
+
+  }
+
   render() {
     return (
       <div className="login-form">
-        <svg viewBox="0 0 300 400" preserveAspectRatio="xMinYMin slice" className="bg" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-          <g>
-            <title>background</title>
-            <rect fill="#fff" id="canvas_background" height="402" width="582" y="-1" x="-1" />
-            <g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
-              <rect fill="url(#gridpattern)" strokeWidth="0" y="0" x="0" height="100%" width="100%" />
-            </g>
-          </g>
-          <g>
-            <title>Layer 1</title>
-            <path id="svg_4" d="m504.5,-57c0,0 -240,298 -240,298c0,0 226,224 226,224c0,0 104,-532 104,-532c0,0 -90,10 -90,10z" strokeOpacity="null" strokeWidth="0" stroke="#000" fill="#f7f7f7" />
-            <path stroke="#000" id="svg_5" d="m4.5,-33c0,0 277.906534,464 277.906534,464c0,0 194.093452,-476 194.093452,-476" opacity="0.5" strokeOpacity="null" strokeWidth="0" fill="#f4f4f4" />
-            <path id="svg_6" d="m140.5,-13c-8,40 -130,468 -130,468c0,0 148,6 148,6c0,0 -10,-514 -18,-474z" fillOpacity="null" strokeOpacity="null" strokeWidth="0" stroke="#000" fill="#f7f7f7" />
-          </g>
-        </svg>
         <div>
-          <h3>Login</h3>
+          <img src="./images/mantle-logo.svg" />
           <form action="" name="login">
-            <TextField fullWidth={true} floatingLabelText="Username" type="text" name="username" id="login-user" />
-            <TextField fullWidth={true} floatingLabelText="Password" type="password" name="password" id="login-pass" />
+            <TextField
+              value={this.state.user}
+              onChange={( e, text ) => this.setState( { pass: text } )}
+              fullWidth={true}
+              floatingLabelText="Username"
+              type="text" name="username"
+              id="login-user" />
+            <TextField
+              value={this.state.pass}
+              onChange={( e, text ) => this.setState( { user: text } )}
+              fullWidth={true}
+              floatingLabelText="Password"
+              type="password" name="password"
+              id="login-pass" />
             <div className="buttons">
               <RaisedButton
                 label="Login"
@@ -32,7 +41,7 @@ export class LoginForm extends React.Component<any, any> {
                 icon={<FontIcon className="fa fa-sign-in" />}
                 primary={true} />
               <div className="anchor-btns">
-                <a href="/register">Create an Account</a> | <a href="">Retrieve Password</a>
+                <a href="/register">Create an Account</a> | <a href="" onClick={e => this.retrievePass()}>Retrieve Password</a>
               </div>
             </div>
           </form>
