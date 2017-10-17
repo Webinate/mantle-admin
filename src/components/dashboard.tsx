06900ce25e3ecf22ae18dba4f76fb709c39d4222
Mathew Henson <mat@webinate.net>
2017-10-17 23:16:54 +0100

    Converted dashboard to use css grid

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -35,42 +35,47 @@ export class Dashboard extends React.Component<Prop, any> {
           />
           <h1>{this.props.title}</h1>
         </Head>
-        <Body>
-          <Menu>
-            <List style={{ padding: '0' }}>
-              {this.props.items.map( ( i, index ) => {
-                return <ListItem
-                  className={this.props.activePath === i.path ? 'selected' : ''}
-                  key={`menu-item-${ index }`}
-                  onClick={e => i.onClick()}
-                  primaryText={i.label}
-                  leftIcon={<FontIcon style={{ color: 'inherit', transition: '' }} className={i.icon}
-                  />} />
-              } )
-              }
-            </List>
-          </Menu>
-          <Content>
-            {this.props.children}
-          </Content>
-        </Body>
+        <Menu>
+          <List style={{ padding: '0' }}>
+            {this.props.items.map( ( i, index ) => {
+              return <ListItem
+                className={this.props.activePath === i.path ? 'selected' : ''}
+                key={`menu-item-${ index }`}
+                onClick={e => i.onClick()}
+                primaryText={i.label}
+                leftIcon={<FontIcon style={{ color: 'inherit', transition: '' }} className={i.icon}
+                />} />
+            } )
+            }
+          </List>
+        </Menu>
+        <Content>
+          {this.props.children}
+        </Content>
+        <Properties>
+        </Properties>
+
       </DashboardOuter>
     )
   }
 }
 
 const DashboardOuter = styled.div`
+  display: grid;
+  grid-template-columns: minmax(100px, 1fr) 1fr 1fr minmax(100px, 1fr);
+  grid-template-rows: 60px 1fr;
+  grid-template-areas:
+    "header header header header"
+    "sections main main props";
   height: 100%;
-  position: relative;
 `;
 
 const Head = styled.div`
   background: ${theme.primary200.background };
   color: ${theme.primary200.color };
   border-bottom: 1px solid ${theme.primary200.border! };
-  height: 60px;
-  position: relative;
   box-sizing: border-box;
+  grid-area: header;
 
   > * {
     display: inline-block;
@@ -84,37 +89,34 @@ const Head = styled.div`
 `;
 
 const Menu = styled.div`
-  float: left;
-  width: 200px;
-  height: 100%;
-  box-sizing: border-box;
+  grid-area: sections;
   background: ${theme.light100.background };
-  border-right: 1px solid ${theme.light100.border! }
+  border-right: 1px solid ${theme.light100.border! };
+  overflow: auto;
+
+  > div > div .selected {
+    color: ${ theme.secondary200.color } !important;
+    background: ${ theme.secondary200.background } !important;
+  }
+
+  > div > div .selected::before {
+    content: '';
+    position: absolute;
+    height: 100%;
+    box-sizing: border-box;
+    border-left: 8px solid ${ theme.secondary100.background };
+  }
 `;
 
-const Body = styled.div`
-height: calc(100% - 60px);
-background: ${ theme.light200.background };
-
-${ Menu } .selected {
-  color: ${ theme.secondary200.color } !important;
-  background: ${ theme.secondary200.background } !important;
-}
-
-${ Menu } .selected::before {
-  content: '';
-  position: absolute;
-  height: 100%;
-  box-sizing: border-box;
-  border-left: 8px solid ${ theme.secondary100.background };
-}
+const Properties = styled.div`
+  grid-area: props;
 `;
 
 const Content = styled.div`
-  float: left;
-  width: calc(100% - 200px);
-  height: 100%;
+  grid-area: main;
   overflow: auto;
   padding: 0 10px;
   box-sizing: border-box;
+  box-shadow: 0px 1px 10px 1px rgba(0,0,0,0.2) inset;
+  background: ${ theme.light200.background } !important;
 `;
