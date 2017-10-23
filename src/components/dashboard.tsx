c073269b1085ac83cce38fa5e17de56e3d2eb87d
Mathew Henson <mat@webinate.net>
2017-10-23 12:18:47 +0100

    Implemented Stage component

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -2,11 +2,10 @@ import * as React from 'react';
 import { IconButton } from 'material-ui';
 import { default as styled } from '../theme/styled';
 import { default as theme } from '../theme/mui-theme';
+import { Stage } from './stage';
 
 type Prop = {
   title: string;
-  rightOpen?: boolean;
-  leftOpen?: boolean;
   onHome: () => void;
   onLogOut: () => void;
   renderLeft?: () => JSX.Element;
@@ -15,45 +14,18 @@ type Prop = {
 
 export class Dashboard extends React.Component<Prop, any> {
 
-  static defaultProps: Partial<Prop> = {
-    rightOpen: true,
-    leftOpen: true
-  }
-
   constructor() {
     super();
   }
 
-  getGridLayout() {
-    if ( this.props.rightOpen && this.props.leftOpen )
-      return {
-        gridTemplateColumns: 'minmax(100px, 1fr) 1fr 1fr minmax(100px, 1fr)',
-        gridTemplateAreas: '"header header header header" "sections main main props"'
-      };
-    else if ( this.props.leftOpen )
-      return {
-        gridTemplateColumns: 'minmax(100px, 1fr) 1fr 1fr',
-        gridTemplateAreas: '"header header header" "sections main main"'
-      };
-    else if ( this.props.rightOpen )
-      return {
-        gridTemplateColumns: '1fr 1fr minmax(100px, 1fr)',
-        gridTemplateAreas: '"header header header" "main main props"'
-      };
-    else
-      return {
-        gridTemplateColumns: '1fr',
-        gridTemplateAreas: '"header" "main"'
-      };
-  }
-
   render() {
+    const headerHeight = 60;
+
     return (
-      <DashboardOuter
-        className="mt-dashboard"
-        style={this.getGridLayout()}
-      >
-        <Head>
+      <DashboardOuter className="mt-dashboard">
+        <Head
+          style={{ height: `${ headerHeight }px` }}
+        >
           <IconButton
             className="mt-logout"
             style={{ color: 'inherit', margin: '5px', float: 'right' }}
@@ -69,21 +41,22 @@ export class Dashboard extends React.Component<Prop, any> {
           />
           <h1>{this.props.title}</h1>
         </Head>
-        {this.props.leftOpen ? <Menu>{this.props.renderLeft && this.props.renderLeft()}</Menu> : undefined}
-
-        <Content>
-          {this.props.children}
-        </Content>
-        {this.props.rightOpen ? <Properties>{this.props.renderRight && this.props.renderRight()}</Properties> : undefined}
-
+        <Stage
+          rightOpen={false}
+          style={{ height: `calc(100% - ${ headerHeight }px)` }}
+          renderLeft={() => <Menu>{this.props.renderLeft && this.props.renderLeft()}</Menu>}
+          renderRight={() => <Properties>{this.props.renderRight && this.props.renderRight()}</Properties>}
+        >
+          <Content>
+            {this.props.children}
+          </Content>
+        </Stage>
       </DashboardOuter>
     )
   }
 }
 
 const DashboardOuter = styled.div`
-  display: grid;
-  grid-template-rows: 60px 1fr;
   height: 100%;
 `;
 
@@ -92,7 +65,6 @@ const Head = styled.div`
   color: ${theme.primary200.color };
   border-bottom: 1px solid ${theme.primary200.border! };
   box-sizing: border-box;
-  grid-area: header;
 
   > * {
     display: inline-block;
@@ -106,10 +78,10 @@ const Head = styled.div`
 `;
 
 const Menu = styled.div`
-  grid-area: sections;
   background: ${theme.light100.background };
   border-right: 1px solid ${theme.light100.border! };
   overflow: auto;
+  height: 100%;
 
   > div > div .selected {
     color: ${ theme.secondary200.color } !important;
@@ -126,11 +98,12 @@ const Menu = styled.div`
 `;
 
 const Properties = styled.div`
-  grid-area: props;
+  height: 100%;
+  overflow: auto;
 `;
 
 const Content = styled.div`
-  grid-area: main;
+  height: 100%;
   overflow: auto;
   padding: 0 10px;
   box-sizing: border-box;
