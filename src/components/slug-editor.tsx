0a686b8947a270a60e0922cde0a6810f6f200593
Mathew Henson <mat@webinate.net>
2018-03-30 16:56:20 +0100

    Allow editing of slug component

--- src/components/slug-editor.tsx
+++ src/components/slug-editor.tsx
@@ -6,7 +6,10 @@ type Props = {
   value?: string;
   onChange: ( val: string ) => void;
 }
+
 type State = {
+  editMode: boolean;
+  value?: string;
 }
 
 /**
@@ -16,6 +19,8 @@ export class SlugEditor extends React.Component<Props, State> {
   constructor( props: Props ) {
     super( props );
     this.state = {
+      editMode: false,
+      value: props.value
     }
   }
 
@@ -24,7 +29,51 @@ export class SlugEditor extends React.Component<Props, State> {
       <SlugContainer>
         <strong>Slug: </strong>
         <i>http://host.net/</i>
-        <i className="mt-slug-value">{this.props.value}</i>
+        {!this.state.editMode ? (
+          <span>
+            <i className="mt-slug-value">{this.props.value}</i>
+            {this.props.value!.length > 0 ?
+              <span className="mt-slug-btn mt-edit-slug" onClick={e => {
+                this.setState( {
+                  editMode: true,
+                  value: this.props.value
+                } )
+              }}
+              >Edit</span> : undefined}
+          </span>
+        ) : (
+            <span>
+              <input
+                autoFocus={true}
+                onFocus={e => { e.currentTarget.select() }}
+                name="mt-slug"
+                value={this.state.value}
+                onKeyUp={e => {
+                  if ( e.keyCode === 13 ) {
+                    this.setState( { editMode: false } );
+                    this.props.onChange( this.state.value! );
+                  }
+                }}
+                onChange={e => {
+                  let cleanValue = e.currentTarget.value.replace( /\s+/g, '-' );
+                  cleanValue = cleanValue.replace( /[^a-zA-Z0-9 -]/g, '' );
+                  this.setState( { value: cleanValue } )
+                }}
+              />
+              <span
+                className="mt-slug-btn mt-slug-cancel"
+                onClick={e => this.setState( { editMode: false } )}>
+                Cancel
+              </span>
+              <span
+                className="mt-slug-btn mt-slug-save"
+                onClick={e => {
+                  this.setState( { editMode: false } );
+                  this.props.onChange( this.state.value! );
+                }}>Save
+              </span>
+            </span>
+          )}
       </SlugContainer>
     );
   }
@@ -35,6 +84,24 @@ const SlugContainer = styled.div`
 
   > strong {
     font-style: italic;
+  }
+
+  .mt-slug-value {
+    margin: 0 10px 0 0;
+  }
+
+  .mt-slug-btn {
+    background: ${theme.light100.background };
+    color: ${theme.light100.color };
+    border: 1px solid ${theme.light100.border };
+    padding: 2px 4px;
+    margin: 0 2px;
+    border-radius: 2px;
+    cursor: pointer;
+
+    &:hover, &:active {
+      border: 1px solid ${theme.primary100.border };
+    }
   }
 
   .mt-slug-value {
