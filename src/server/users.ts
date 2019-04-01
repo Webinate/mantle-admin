b143d603dab3c3e75e2291682d0af1f4933257d8
Mathew Henson <mat@webinate.net>
2019-04-02 00:10:14 +0100

    Updated to use latest graphql code

--- src/server/users.ts
+++ src/server/users.ts
@@ -3,6 +3,6 @@ import { controllers } from '../../../../src';
 import { ActionCreators as UserActions } from '../store/users/actions';
 
 export default async function(actions: Action[]) {
-  const users = await controllers.users.getUsers(0, 10);
+  const users = await controllers.users.getUsers({ index: 0, limit: 10 });
   actions.push(UserActions.SetUsers.create(users));
 }
